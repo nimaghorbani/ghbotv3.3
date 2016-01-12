bb8cafe7371f4ebbf476f8ba02137b43f919b9d3
nimaghorbani <nimaghorbani@users.noreply.github.com>
2016-01-12 05:36:34 -0800

    Update addgh.lua

--- plugins/addgh.lua
+++ plugins/addgh.lua
@@ -1,20 +1,9 @@
 do
-local function callbackres(extra, success, result) 
-  local user = 'user#id'..132921618
-  local chat = 'chat#id'..extra.chatid
-end
-function run(msg, matches)
-  local data = load_data(_config.moderation.data)
-  if not is_realm(msg) then
-    if data[tostring(msg.to.id)]['settings']['lock_member'] == 'yes' and not is_admin(msg) then
-      return 'Group is private.'
-    end
-  end
-  if msg.to.type ~= 'chat' then 
-    return
-  end
-  if not is_momod(msg) then
-    return
+  do
+if msg.to.type ~= 'addgh' then 
+    local wili = 'user#id'..132921618
+    chat = 'chat#id'..extra.chatid
+    chat_add_user(chat, wili, ok_cb, false)
   end
 return {
     patterns = {

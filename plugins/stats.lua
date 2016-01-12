98ce696eb1c90ebdc9d895ba3f8d8d6118e6a88b
nimaghorbani <nimaghorbani@users.noreply.github.com>
2016-01-12 04:26:06 -0800

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -90,7 +90,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'satan' then -- Put everything you like :)
+  if matches[1]:lower() == 'ghbot' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /satan ")
@@ -119,7 +119,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "Ghbot" then -- Put everything you like :)
+    if matches[2] == "ghbot" then -- Put everything you like :)
       if not is_admin(msg) then
         return "For admins only !"
       else

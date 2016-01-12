44a9708675f141361042684362df178d99848103
nimaghorbani <nimaghorbani@users.noreply.github.com>
2016-01-12 04:31:28 -0800

    Update helppv.lua

--- plugins/helppv.lua
+++ plugins/helppv.lua
@@ -1,6 +1,6 @@
 do
     function run(msg, matches)
-	return 'For More Information Send PM \n @MrGhor \n برای اطلاعات بیشتر به اکانت زیر یک پیام بفرستید  \n  @@MrGhor'
+	return 'For More Information Send PM \n @MrGhor \n Link Suport Gh Bot : \n https://telegram.me/joinchat/B-w5EgPXU5EZOP96pQhOqg '
 end
 return {
   patterns = {

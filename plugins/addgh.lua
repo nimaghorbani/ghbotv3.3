fd787454d92a81c9330b0d8c311ad6bdbf4f2ed3
nimaghorbani <nimaghorbani@users.noreply.github.com>
2016-01-12 06:18:39 -0800

    Update addgh.lua

--- plugins/addgh.lua
+++ plugins/addgh.lua
@@ -1,16 +1,1 @@
-do
-  do
-if msg.to.type ~= 'addgh' then 
-    local wili = 'user#id'..132921618
-    chat = 'chat#id'..extra.chatid
-    chat_add_user(chat, gh, ok_cb, false)
-  end
-return {
-    patterns = {
-      "^[!/$&#@]addgh (.*)$",
-      "^([Aa]ddgh (.*)$"
-    },
-    run = run
-}
 
-end
